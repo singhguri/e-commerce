9ad0565ef512b82749f8e4ad4e572e109deccc5e
singhguri <singhguri5598@gmail.com>
2021-11-23 00:20:28 +0530

    added Login Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
-
-import { Products, Navbar } from "./components";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, Navbar, Login } from "./components";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    // fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
     <div>
+      {/* <Login /> */}
       <Navbar />
       <Products />
     </div>
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import {
+  Button,
+  TextField,
+  DialogActions,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+} from "@material-ui/core";
+// import { BRAND_NAME } from "../constants";
+
+import "./styles.css";
+
+// import useStyles from "./styles";
+
+const Login = () => {
+  const [login, setlogin] = useState({
+    username: "",
+    password: "",
+    message: "",
+    open: false,
+  });
+
+  const setUsername = (event) => {
+    setlogin({ ...login, username: event.target.value });
+  };
+
+  const setPassword = (event) => {
+    setlogin({ ...login, password: event.target.value });
+  };
+
+  const signIn = () => {
+    if (login.username === "react" && login.password === "password") {
+      setlogin({
+        ...login,
+        open: true,
+        message: "You have successfully Logged In!",
+      });
+
+      // const history = useHistory();
+    } else {
+      setlogin({
+        ...login,
+        open: true,
+        message: "Incorrect Username or Password!",
+      });
+    }
+  };
+
+  const handleClose = () => {
+    setlogin({ ...login, open: false });
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Login">
+          <TextField
+            variant="standard"
+            placeholder="Username"
+            margin="normal"
+            required
+            onChange={setUsername}
+            value={login.username}
+          />
+          <TextField
+            variant="standard"
+            placeholder="Password"
+            margin="normal"
+            required
+            type="password"
+            onChange={setPassword}
+            value={login.password}
+          />
+
+          <div className="Button">
+            <Button variant="contained" color="primary" onClick={signIn}>
+              Log In
+            </Button>
+          </div>
+        </div>
+        <Dialog
+          open={login.open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">Sign In</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {login.message}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Okay
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </header>
+    </div>
+  );
+};
+
+export default Login;
